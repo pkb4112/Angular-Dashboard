c89e35bf55d7a501ad2bcb2c228b57b225c7847e
Peter Brandon <peterkbrandon@gmail.com>
2018-03-09 18:33:17 -0500

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { DocumentsComponent } from './documents/documents.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    DocumentsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+  AppComponent
+  ]
 })
 export class AppModule { }
