af291d1f332524b12caaf65aab7ee04cb5d6cf6e
Peter Brandon <peterkbrandon@gmail.com>
2018-03-09 18:50:46 -0500

    Documents list

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -5,7 +5,34 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './documents.component.html',
   styleUrls: ['./documents.component.css']
 })
+
 export class DocumentsComponent implements OnInit {
+  
+  documents: Document[] = [
+
+    { 
+    	title: "First Doc",
+    	description: "stuff stuff stuff first doc",
+    	file_url: "http://google.com",
+    	updated_at: 'date-holder',
+    	image_url: 'http://google.com/images'
+    },
+    { 
+    	title: "Second Doc",
+    	description: "stuff stuff stuff second doc",
+    	file_url: "http://google.com",
+    	updated_at: 'date-holder',
+    	image_url: 'http://google.com/images'
+    },
+    { 
+    	title: "Third Doc",
+    	description: "stuff stuff stuff third doc",
+    	file_url: "http://google.com",
+    	updated_at: 'date-holder',
+    	image_url: 'http://google.com/images'
+    }
+
+  ]
 
   constructor() { }
 
@@ -13,3 +40,5 @@ export class DocumentsComponent implements OnInit {
   }
 
 }
+
+
